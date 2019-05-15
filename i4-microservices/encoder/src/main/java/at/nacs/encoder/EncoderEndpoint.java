437240c87817697b0f253e2b93a0cec466309a03
HosamZ <hsmzaror@gmail.com>
2019-05-15 13:46:45 +0200

    removed unwanted lines

--- i4-microservices/encoder/src/main/java/at/nacs/encoder/EncoderEndpoint.java
+++ i4-microservices/encoder/src/main/java/at/nacs/encoder/EncoderEndpoint.java
@@ -1,21 +1,26 @@
 package at.nacs.encoder;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/encode")
 @RequiredArgsConstructor
 public class EncoderEndpoint {
 
-    private final Encoder encoder;
+  private final Encoder encoder;
 
-    @PostMapping
-    String encode(@RequestBody String message) {
-        return encoder.encode(message);
-    }
+  private final EncoderClient encoderClient;
 
+  @PostMapping
+  String encode(@RequestBody String message) {
+    return encoder.encode(message);
+  }
+
+  @GetMapping
+  List<String> encode() {
+    return encoderClient.encode();
+  }
 }
